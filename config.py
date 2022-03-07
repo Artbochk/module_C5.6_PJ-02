3dc69e4b6db487436544a4e64652ed1a9d481530
Artbochk <98022578+Artbochk@users.noreply.github.com>
2022-03-07 16:34:15 +0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = '5209307440:AAHN4tTH-Ekjdq0PDbqMn2uxPEWymMkIQZU'
-
-access_key = '0344930c82db90319950c3eb1b2b6440'
-
-keys = {
-    'рубль': 'RUB',
-    'евро': 'EUR',
-    'доллар': 'USD'
-}
